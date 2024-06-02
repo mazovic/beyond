fa26590eb5948b11cb382a0291cf2f388102dd4a
mazovic <mazovic23@gmail.com>
2024-06-02 23:10:40 +0300

    email is ready but not tested yet

--- src/website/sendEmailController.js
+++ src/website/sendEmailController.js
@@ -1,4 +1,5 @@
 const contactUs = require("../admin/models/contactUsModel");
+const sendEmail = require("./../utils/email");
 exports.createMessage = async (req, res, next) => {
   const {
     name,
@@ -27,6 +28,12 @@ exports.createMessage = async (req, res, next) => {
       institution,
       bestTime,
     });
+    await sendEmail({
+      sender: "name",
+      email: "the email we want to send to",
+      subject: requestType,
+      message: `${name} want to contact us and his email is ${email} about ${requestType} and he hear about us from ${HowDidYouHearAboutUs}`,
+    });
     res
       .status(201)
       .json({ status: "success", message: "Request has sent successfully" });
@@ -34,6 +41,6 @@ exports.createMessage = async (req, res, next) => {
     console.log(err);
     res
       .status(400)
-      .json({ status: "fail", message: "Error Reqesting a message" });
+      .json({ status: "fail", message: "email didn't sent successfully" });
   }
 };
